51ad41025b3a2c677296150fbb2c2cc461a1e9eb
lombardoh <lombardo.h@hotmail.com>
2024-04-19 23:02:19 -0300

    initial commit

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    InputManager inputManager;
+
+    public Transform targetTransform;
+    public Transform cameraPivot;
+    private Vector3 cameraFollowVelocity = Vector3.zero;
+
+    public float cameraFollowSpeed = 0.2f;
+    public float cameraLookSpeed = 2;
+    public float cameraPivotSpeed = 2;
+
+    public float lookAngle;
+    public float pivotAngle;
+    public float minimunPivotAngle = -35;
+    public float MaximumPivotAngle = 35;
+
+    private void Awake()
+    {
+        inputManager = FindObjectOfType<InputManager>();
+        targetTransform = FindObjectOfType<PlayerManager>().transform;
+    }
+
+    public void HandleAllCameraMovement()
+    {
+        FollowTarget();
+        RotateCamera();
+    }
+    public void FollowTarget()
+    {
+        Vector3 targetPosition = Vector3.SmoothDamp(transform.position, targetTransform.position, ref cameraFollowVelocity, cameraFollowSpeed );
+        transform.position = targetPosition;
+    }
+
+    public void RotateCamera()
+    {
+        Vector3 rotation;
+        Quaternion targetRotation;
+
+        lookAngle = lookAngle + (inputManager.cameraInputX * cameraLookSpeed);
+        pivotAngle = pivotAngle - (inputManager.cameraInputY * cameraPivotSpeed);
+        pivotAngle = Mathf.Clamp(pivotAngle, minimunPivotAngle, MaximumPivotAngle);
+
+        rotation = Vector3.zero;
+        rotation.y = lookAngle;
+        targetRotation = Quaternion.Euler(rotation);
+        transform.rotation = targetRotation;
+
+        rotation = Vector3.zero;
+        rotation.x = pivotAngle;
+        targetRotation = Quaternion.Euler(rotation);
+        cameraPivot.localRotation = targetRotation;
+    }
+
+    private void HandleCameraCollisions()
+    {
+
+    }
+}
